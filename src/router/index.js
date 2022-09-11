a3bea045db853a6f0fd32d4e277d5562996cbb3f
tangchaolin <tcl1043@163.com>
2022-09-11 10:56:26 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: ()=>import("@/views/Home")
+  }
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
