f498a5a313adac6bb078bf94c666f1c57db45719
tangchaolin <tcl1043@163.com>
2022-09-11 11:10:54 +0800

    实现了公告板功能

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    <div class="box">🔔{{billboard}}</div>
+
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  data(){
+
+    return{
+      billboard:"版本更新"
+    }
+  }
+}
+</script>
