14912ecbf8d8983f7ff88acd1f5a834034495270
tangchaolin <tcl1043@163.com>
2022-09-12 14:54:27 +0800

    每日一句

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'buefy/dist/buefy.css'
 //element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/app.css'
 Vue.use(buefy)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
---
+++ src/views/card/Promotion.vue
@@ -0,0 +1,29 @@
+<template>
+<div style="margin-bottom:16px">
+  <el-card class="box-card" shadow="hover">
+    <div slot="header">
+      <span>🥂 推广</span>
+    </div>
+    <div>
+      body
+    </div>
+  </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Promotion',
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style scoped>
+</style>
