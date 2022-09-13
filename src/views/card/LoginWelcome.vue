58d9b9a9bae3538368ea6b6e365713431d3ad875
tangchaolin <tcl1043@163.com>
2022-09-14 01:29:36 +0800

    前端侧边栏，马上入驻，社区登入功能

--- src/views/card/LoginWelcome.vue
+++ src/views/card/LoginWelcome.vue
@@ -4,19 +4,30 @@
     <div slot="header">
       <span>💐 发帖</span>
     </div>
-    <div>
-      body
+    <div v-if="token != null && token !== ''" class="has-text-centered">
+      <b-button type="is-danger" tag="router-link" :to="{path:'/post/create'}" outlined>✍ 发表想法</b-button>
+    </div>
+
+    <div v-else class="has-text-centered">
+      <b-button type="is-primary" tag="router-link" :to="{path:'/register'}" outlined>马上入驻</b-button>
+      <b-button type="is-danger" tag="router-link" :to="{path:'/login'}" outlined class="ml-2"> 社区登入</b-button>
     </div>
   </el-card>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'LoginWelcome',
   data() {
     return {}
   },
+    computed: {
+    ...mapGetters([
+      'token'
+    ])
+  }
 }
 </script>
 
